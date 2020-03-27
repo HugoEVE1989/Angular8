c8ae8031641ee68e67f33b26085e0640cd877171
hugo eloy vargas escorcia <B197069@EKT1673534MACLAP.local>
2020-03-26 18:59:29 -0700

    Se actualiza proyecto de preguntas con sus respectivas notas

--- questionario_app/cuestionario-app/src/app/components/test/test.component.ts
+++ questionario_app/cuestionario-app/src/app/components/test/test.component.ts
@@ -7,9 +7,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  constructor() { }
+  public question: string; 
+  public answers: any[];
+  public answerCorrect: boolean;
+  public answerChoosed:boolean;
+
+  constructor() { 
+    this.question='¿Cúal es la capital de españa?';
+    this.answers=[{
+      'answer' : 'Zaragoza',
+      'correct': false
+    },
+    {
+      'answer' : 'Barcelona',
+      'correct': false
+    },
+    {
+      'answer' : 'Madrid',
+      'correct': true
+    },
+    {
+      'answer' : 'Sevilla',
+      'correct': false
+    }
+  ];
+
+  this.answerCorrect=false;
+  this.answerChoosed=false;
+
+  }
 
   ngOnInit() {
+  }
+
+  checkAnswerIndex(index){
+    this.answerCorrect=this.answers[index].correct;
+    this.answerChoosed=true;
+  }
+
+  checkAnswerObj(obj){
+    this.answerCorrect=obj.correct;
+    this.answerChoosed=true;
+  }
+
+  checkAnswerQuestion(){
+    if(!this.question){
+      return false;
+    }
+
+    if(!this.answers || !Array.isArray(this.answers)){
+      return false;
+    }else{
+
+      if(!(this.answers.length >= 2 && this.answers.length<=4)){
+        return false;
+      }
+
+
+      const correctAnswers = this.answers.filter(answer => answer.correct);
+      
+      /*
+      const correctAnswers=this.answers.filter(function (answer){
+        return answer.correct;
+      } 
+        
+      }*/
+
+      if(correctAnswers.length !== 1){
+        return false;
+      }
+
+    }
+
+    return true;
+
   }
 
 }
