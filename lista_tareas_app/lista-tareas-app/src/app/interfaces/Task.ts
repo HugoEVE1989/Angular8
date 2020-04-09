51e458b53b529d105eada8dfe17d553a199f7442
hugo eloy vargas escorcia <B197069@EKT1673534MACLAP.local>
2020-04-09 08:57:03 -0700

    Se actualiza proyecto hasta video 44 de Udemy

---
+++ lista_tareas_app/lista-tareas-app/src/app/interfaces/Task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+    'date':Date,
+    'description': string,
+    'completed' : boolean
+    //declarar una variable opcional se agrega un ? al final del nombre ej 'opcional?':boolean
+}
